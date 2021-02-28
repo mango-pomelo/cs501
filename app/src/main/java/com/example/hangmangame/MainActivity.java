cd7d50b3167634a62fc7852db1552a0022989512
Rachel Peng <rachelvpeng@gmail.com>
2021-02-27 23:59:45 -0500

    more game functionality

--- app/src/main/java/com/example/hangmangame/MainActivity.java
+++ app/src/main/java/com/example/hangmangame/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -31,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
     String word;
     String hint1;
     String hint2;
-    boolean[] chosen;
+
+    boolean[] letterChosen;
+
+    int wins;
+    int losses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         llMain = findViewById(R.id.llMain);
 
         alphabetButtons = (findViewById(R.id.tableLayout)).getTouchables();
+
+        wins = 0;
+        losses = 0;
 
         words = new Hashtable<String, String[]>();
         String[] hints1 = {getResources().getString(R.string.hint1_1), getResources().getString(R.string.hint1_2)};
@@ -72,16 +80,80 @@ public class MainActivity extends AppCompatActivity {
         Button buttonSelected = (Button) findViewById(view.getId());
         String letter = buttonSelected.getText().toString();
 
-        for (int i = 0; i < chosen.length; i++) {
+        buttonSelected.setClickable(false);
+        buttonSelected.setTextColor(Color.GRAY);
+
+        boolean correctLetter = false;
+
+        for (int i = 0; i < word.length(); i++) {
             if (word.substring(i, i + 1).equals(letter)) {
-                chosen[i] = true;
                 txtWord[i].setText(word.substring(i, i+1));
+                correctLetter = true;
+                letterChosen[i] = true;
             }
         }
 
-        buttonSelected.setClickable(false);
-        buttonSelected.setTextColor(Color.GRAY);
+        if (!correctLetter) {
+            nextDrawable();
+        } else if (hasWon()) {
+            endGame("win");
+        }
     }
+
+    boolean hasWon() {
+        for (int i = 0; i < letterChosen.length; i++) {
+            if (!letterChosen[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void nextDrawable() {
+        int theID = (Integer) img.getTag();
+        switch(theID){
+            case R.drawable.hangman_0:
+                img.setTag(R.drawable.hangman_1);
+                img.setImageResource(R.drawable.hangman_1);
+                break;
+            case R.drawable.hangman_1:
+                img.setTag(R.drawable.hangman_2);
+                img.setImageResource(R.drawable.hangman_2);
+                break;
+            case R.drawable.hangman_2:
+                img.setTag(R.drawable.hangman_3);
+                img.setImageResource(R.drawable.hangman_3);
+                break;
+            case R.drawable.hangman_3:
+                img.setTag(R.drawable.hangman_4);
+                img.setImageResource(R.drawable.hangman_4);
+                break;
+            case R.drawable.hangman_4:
+                img.setTag(R.drawable.hangman_5);
+                img.setImageResource(R.drawable.hangman_5);
+                break;
+            case R.drawable.hangman_5:
+                img.setTag(R.drawable.hangman_6);
+                img.setImageResource(R.drawable.hangman_6);
+
+                endGame("loss");
+                break;
+        }
+    }
+
+    void endGame(String outcome) {
+        disableButtons();
+        String score = "Current score: " + wins + " wins -- " + losses + " losses";
+
+        if (outcome.equals("win")) {
+            wins += 1;
+            Toast.makeText(this, "You win! " + score, Toast.LENGTH_LONG).show();
+        } else {
+            losses += 1;
+            Toast.makeText(this, "You lose! " + score, Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     private void enableButtons() {
         for (View v : alphabetButtons) {
@@ -92,10 +164,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void disableButtons() {
+        for (View v : alphabetButtons) {
+            if (v instanceof Button) {
+                ((Button) v).setClickable(false);
+                ((Button) v).setTextColor(Color.GRAY);
+            }
+        }
+    }
+
     protected void createNewGame() {
         enableButtons();
+
         llMain.removeAllViewsInLayout();
 
+        img.setTag(R.drawable.hangman_0);
         img.setImageResource(R.drawable.hangman_0);
 
         String[] keys = words.keySet().toArray(new String[words.size()]);
@@ -103,9 +186,9 @@ public class MainActivity extends AppCompatActivity {
         hint1 = words.get(word)[0];
         hint2 = words.get(word)[1];
 
-        chosen = new boolean[word.length()];
-        for (int i = 0; i < chosen.length; i++) {
-            chosen[i] = false;
+        letterChosen = new boolean[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            letterChosen[i] = false;
         }
 
         llParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
